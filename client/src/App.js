0b85933025872e20cc8c3723a3a13cfbc65ead3d
DKProtocol <damann1072@gmail.com>
2023-01-11 15:59:22 -0600

    Initial

---
+++ client/src/App.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState([]);
+  const [year, setYear] = useState('2009');
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  const handleYear = event => {
+    setYear(event.target.value);
+  }
+
+  const getMovies = () => {
+    fetch(`http://localhost:5000/movies?key=year&value=${year}`)
+      .then(res => res.json())
+      .then(details => {
+        setData(details);
+        setMessage("Try a different year: Some years don't have movies!")
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  return (
+    <div className="App">
+      <h2>Movie List</h2>
+
+      <p>Sample dataset supplied by MongoDB for award winning movies.</p>
+      <p>Search below by year and top movies from that time period will be pulled from the database.</p>
+
+      <div className='inputs'>
+        <input onChange={handleYear} placeholder='Year (1893 - 2016)'></input>
+        <button onClick={getMovies}>Search</button>
+      </div>
+
+      <h4>{year ? `Year: ${year} - Movies: ${data.length}` : ""}</h4>
+
+      <div className='card-container'>
+        {data.length === 0
+          ? <p>{message}</p>
+          : data.map((details, index) => (
+            <div key={index} className='card'>
+              <h4>{details.title}</h4>
+              <p>{details.released ? `Date: ${details.released.split("-")[1]}/${details.released.split("-")[2].split("T")[0]}` : ""}</p>
+
+              {/* All of these ternary operators are checking for undefineds in each movie object */}
+              {
+                details.tomatoes
+                  ? details.tomatoes.boxOffice ? <p>{`Box Office: ${details.tomatoes.boxOffice}`}</p> : ""
+                  : ""
+              }
+              <p>{
+                details.tomatoes
+                  ? details.tomatoes.critic ? `Ratings - IMDB: ${details.imdb.rating} / Tomatoes: ${details.tomatoes.critic.meter}` : `Ratings - IMDB: ${details.imdb.rating}`
+                  : `Ratings - IMDB: ${details.imdb.rating}`
+              }</p>
+              <p>{`Awards: ${details.awards.text}`}</p>
+              <p>{
+                details.cast
+                  ? `Cast: ${details.cast.join(', ')}`
+                  : ""
+              }</p>
+              <img src={details.poster} alt='* No movie poster *' className='poster'></img>
+              {
+                details.fullplot
+                  ? <p>{details.fullplot}</p>
+                  : <p>* Description not available *</p>
+              }
+            </div>
+          ))}
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,40 @@
+const { MongoClient } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send({ "Server Status:": "Server is running :)" })
+});
+
+app.get('/movies', async (req, res) => {
+    const client = new MongoClient(process.env.URI);
+    try {
+        const key = req.query.key;
+        const val = req.query.value;
+        console.log(key + " " + val);
+        const database = client.db('sample_mflix');
+        const movies = database.collection('movies');
+        const cursor = await movies.find({ year: parseInt(val) }).toArray();
+        res.json(cursor);
+    } finally {
+        await client.close();
+    }
+})
+
+app.get('/test', (req, res) => {
+    const body = req.query;
+    const param = req.params
+    console.log(body);
+    console.log(param);
+})
+
+const port = 5000;
+app.listen(port, () => {
+    console.log(`Server is listening on port: ${port}`)
+});
