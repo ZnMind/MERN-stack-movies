e94afb8d5f24153dbf532fe423412001734a1313
DKProtocol <damann1072@gmail.com>
2023-01-11 16:31:15 -0600

    Cors origin

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
